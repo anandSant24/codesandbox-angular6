7aebda38f2a355d29e239c37b2a007ffe7b0dee6
anandSant24 <anandSant24@users.noreply.github.com>
2019-11-22 22:34:08 -0500

    Interview Coding
    display the list of values added to the input array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  result: Array<string> = [];
   title = "CodeSandbox";
+  inputText: string = "Enter user details";
+  addItemsToArray(): void {
+    console.log(this.inputText);
+    this.result.push(this.inputText);
+  }
 }
