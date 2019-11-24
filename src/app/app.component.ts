93c6b52fea2b747ecc22ee63b25224261485b6b5
anandSant24 <anandSant24@users.noreply.github.com>
2019-11-23 19:08:42 -0500

    Interpolation, Property Binding and DOM Property vs Attribute binding
    DOM Property vs Attribute 
    DOM property values can changes where as attribute of an element won't change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from "@angular/core";
-
+import { Component, Input } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -9,8 +8,32 @@ export class AppComponent {
   result: Array<string> = [];
   title = "CodeSandbox";
   inputText: string = "Enter user details";
+
   addItemsToArray(): void {
     console.log(this.inputText);
     this.result.push(this.inputText);
+  }
+  /* interpolation is used to concatenate string */
+  imageHttp: string = "https";
+  isEnable: Boolean = false;
+  isDisabled: Boolean = false;
+  /* ##10 Dom Property vs HTML element attribute */
+  /*  When a Browser loads a web page the browser create a DOM for that page
+    Attributes are defined by HTML where as 
+    Properties are defined by DOM
+
+    Attributes initializes DOM properties, Once initializtion complete the attribute job is done.
+
+    Property values can Change whereas
+    Attribute Value Can't
+*/
+  private _id;
+  @Input()
+  set id(value: string) {
+    this._id = value;
+    console.log(this._id);
+  }
+  get id() {
+    return this._id;
   }
 }
