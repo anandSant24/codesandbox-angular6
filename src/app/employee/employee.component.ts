35d467d0507e5093eb2a5619ac72871cd6e00565
anandSant24 <anandSant24@users.noreply.github.com>
2019-11-24 17:35:00 -0500

    13 Class Binding in Angular
    how to bind class and multiple class in Angular

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -2,8 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "employee-table",
-  templateUrl: "employee.component.html"
+  templateUrl: "employee.component.html",
+  styleUrls: ["employee.component.css"]
 })
 export class EmployeeComponent {
   columnSpan: number = 2;
+  styleClass: string = "italicClass boldClass";
+  isBoldClass: boolean = true;
+  isItalicClass: boolean = true;
+  isColor: boolean = false;
+  applyStyles() {
+    let applyClasses = {
+      boldClass: this.isItalicClass,
+      italicClass: this.isBoldClass,
+      colorClass: this.isColor
+    };
+    return applyClasses;
+  }
 }
